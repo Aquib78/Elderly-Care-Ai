cee4411a99d8f2f08c257c6fcd5f3ffb13733f48
Heet Bhudia <heetbhudia@gmail.com>
2026-04-10 15:45:46 +0530

    feat: added reports API router and integrated with main application

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.user import router as user_router
+from routes.reports import router as report_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(report_router)
